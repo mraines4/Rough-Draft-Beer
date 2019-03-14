1879a87ebc558f397194417087bd8204c05c6e2f
Jonathan Ray <jonathanray17@gmail.com>
2019-03-14 11:28:53 -0400

    adds a function to find the different types of breweries

--- scripts/api.js
+++ scripts/api.js
@@ -1,30 +1,45 @@
-//////////////////////////////
-// OPENBREWERYDB API BEGINS //
-//////////////////////////////
-// const beerURL = './scripts/breweryDB.json';
+/////////////////////////////
+//OPENBREWERYDB API BEGINS //
+/////////////////////////////
+const beerURL = './scripts/breweryDB.json';
 
-// fetch(beerURL) // getting the database data
-// .then(function (response){
-//     return response.json(); // workable data
-// })
-// .then(function (data){
-//     let cleanDB = filterOutPhoneyPhones(filterOutPlanners(data)); // start calling functions
-//     return cleanDB; // return a workable array of objects of breweries with real phone #s that exist
-// });
+fetch(beerURL) // getting the database data
+.then(function (response){
+    return response.json(); // workable data
+})
+.then(function (data){
+    let cleanDB = filterOutPhoneyPhones(filterOutPlanners(data)); // start calling functions
+    return cleanDB; // return a workable array of objects of breweries with real phone #s that exist
+});
 
-// function filterOutPhoneyPhones(breweriesArray){
-//     let goodPhones = breweriesArray.filter(function (eachBrewery){
-//         return ((eachBrewery["phone"]).length === 10); // get rid of brewery with anything but 10 digit phone #s
-//     });
-//     return goodPhones;
-// }
+function filterOutPhoneyPhones(breweriesArray){
+    let goodPhones = breweriesArray.filter(function (eachBrewery){
+        return ((eachBrewery["phone"]).length === 10); // get rid of brewery with anything but 10 digit phone #s
+    });
+    return goodPhones;
+}
 
-// function filterOutPlanners(breweriesArray){
-//     const existingBreweries = breweriesArray.filter(function (eachBrewery){
-//         return ((eachBrewery["brewery_type"]) !== "planning"); // get rid of any brewery still in planning
-//     });
-//     return existingBreweries;
-// }
+function filterOutPlanners(breweriesArray){
+    const existingBreweries = breweriesArray.filter(function (eachBrewery){
+        return ((eachBrewery["brewery_type"]) !== "planning"); // get rid of any brewery still in planning
+    });
+    console.log(existingBreweries);
+    return existingBreweries;
+}
+
+function showMeTheBreweryTypes(breweriesArray){ // quick optional function to look at the different types of breweries
+    let breweryTypesArray =[];
+    breweriesArray.forEach(function (eachBrewery){
+        if (!(breweryTypesArray.includes(eachBrewery["brewery_type"]))){
+            breweryTypesArray.push(eachBrewery["brewery_type"]);
+        }
+        else{
+        }
+    });
+    // console.log('here are your brewery types!');
+    // console.log(breweryTypesArray);
+    return breweryTypesArray;
+}
 
 /////////////////////
 // YELP API BEGINS //
@@ -44,21 +59,26 @@
 //     console.log('that was the data');
 //     // return cleanDB; // return a workable array of objects of breweries with real phone #s that exist
 // });
-geoKey = '9940fdfbec3c42328da75e23977d75a9';
-const GEO_URL = `https://api.opencagedata.com/geocode/v1/json?q=Atlanta,Georgia,US&key=${geoKey}`;
 
-    fetch(GEO_URL)
-        .then(function (response) {
-            // Start the conversion process.
-            return response.json();  // <----- We're returning another Promise.
-        })
-        .then(function (geoData) {
-            console.log('Inside the function (geoData)');
-            console.log(geoData.results[0].geometry.lat);
-            console.log(geoData.results[0].geometry.lng);
-            return geoData.results[0].geometry;
-        })
-        .then(function (coordinates) {
-            console.log('Inside the function (coordinates)');
-            // Call the other API with yet another fetch
-            console.log(coordinates);
+/////////////////////////
+// OPENCAGE API BEGINS //
+/////////////////////////
+// geoKey = '9940fdfbec3c42328da75e23977d75a9';
+// const GEO_URL = `https://api.opencagedata.com/geocode/v1/json?q=Atlanta,Georgia,US&key=${geoKey}`;
+
+// fetch(GEO_URL)
+// .then(function (response) {
+//     // Start the conversion process.
+//     return response.json();  // <----- We're returning another Promise.
+// })
+// .then(function (geoData) {
+//     console.log('Inside the function (geoData)');
+//     console.log(geoData.results[0].geometry.lat);
+//     console.log(geoData.results[0].geometry.lng);
+//     return geoData.results[0].geometry;
+// })
+// .then(function (coordinates) {
+//     console.log('Inside the function (coordinates)');
+//     // Call the other API with yet another fetch
+//     console.log(coordinates);
+// });
